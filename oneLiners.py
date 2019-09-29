163d5520391067bc641c24cb86eba7f5c41bd2ed
Kingsmanvk <vedron007@gmail.com>
2019-09-30 00:19:44 +0530

    Update oneLiners.py

--- oneLiners.py
+++ oneLiners.py
@@ -1,2 +1,7 @@
-# Pangram checker --checks all the alphabets are there in the statement
+# Pangram checker -- checks all the alphabets are there in the statement
 print("pangram") if ''.join(sorted(set(''.join(input().split()).lower())))=="abcdefghijklmnopqrstuvwxyz"  else print("not pangram")
+
+# Anagram Checker -- checks if entered word can be obtained by rearranging the alphabets of another entered word
+print("Anagram") if sorted(input().lower())==sorted(input().lower()) else print("Not Anagram")
+
+
